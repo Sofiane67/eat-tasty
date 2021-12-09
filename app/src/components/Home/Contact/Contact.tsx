d2fca792c11b7ee402836ae960e271fbd7618f2a
Sofiane <sa.dev672@gmail.com>
2021-12-09 20:57:19 +0100

    Ajout de la section Contact à la page Home

--- app/src/components/Home/Contact/Contact.tsx
+++ app/src/components/Home/Contact/Contact.tsx
@@ -2,20 +2,20 @@ import Section from "../../layout/Section/Section";
 import Wrapper from "../../layout/Wrapper/Wrapper";
 import TextSection from "../TextSection/TextSection";
 import ImgSection from "../ImgSection/ImgSection";
-import phoneImg from "../../../images/phone-img.png";
+import storeImg from "../../../images/store-img.png";
 
-const Faq = () => {
+const Contact = () => {
     const text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse sequi quasi placeat voluptatum vitae architecto a repellendus est aperiam aliquam."
     return (
-        <Section nameClass="">
+        <Section nameClass="shape">
             <Wrapper nameClass="grid">
-                <ImgSection image={phoneImg} />
-                <TextSection text={text} textBtn="Voir la FAQ">
-                    <h2 className="heading heading--h2">The home offresh products</h2>
+                <TextSection text={text} textBtn="+333 88 00 00 00">
+                    <h2 className="heading heading--h2">Call our store and takeaway when it suits you best</h2>
                 </TextSection>
+                <ImgSection image={storeImg} />
             </Wrapper>
         </Section>
     )
 }
 
-export default Faq;
+export default Contact;
--- app/src/pages/Home/Home.tsx
+++ app/src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import { Fragment } from "react";
 import About from "../../components/Home/About/About";
+import Contact from "../../components/Home/Contact/Contact";
 import Faq from "../../components/Home/Faq/Faq";
 import Hero from "../../components/Home/Hero/Hero";
 import HowWork from "../../components/Home/HowWork/HowWork";
@@ -12,6 +13,7 @@ const Home = () => {
             <About/>
             <HowWork/>
             <Faq/>
+            <Contact/>
         </Fragment>
         
     )
