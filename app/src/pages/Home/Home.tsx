c1bbe85e5697740e1c3afd1960ed93057acbab21
Sofiane <sa.dev672@gmail.com>
2021-12-09 21:56:57 +0100

    Ajout de la section Call to Action à la pae Home

--- app/src/pages/Home/Home.tsx
+++ app/src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import { Fragment } from "react";
 import About from "../../components/Home/About/About";
 import Contact from "../../components/Home/Contact/Contact";
+import Cta from "../../components/Home/Cta/Cta";
 import Faq from "../../components/Home/Faq/Faq";
 import Hero from "../../components/Home/Hero/Hero";
 import HowWork from "../../components/Home/HowWork/HowWork";
@@ -14,6 +15,7 @@ const Home = () => {
             <HowWork/>
             <Faq/>
             <Contact/>
+            <Cta/>
         </Fragment>
         
     )
